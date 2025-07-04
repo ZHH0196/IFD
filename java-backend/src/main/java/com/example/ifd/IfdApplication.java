89f3c90de4b85cacbbbf47f8e0322b1d12dee623
zhanghaohao <152976058+ZHH0196@users.noreply.github.com>
2025-07-04 21:05:11 +0800

    Start Java + Vue refactor

---
+++ java-backend/src/main/java/com/example/ifd/IfdApplication.java
@@ -0,0 +1,11 @@
+package com.example.ifd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfdApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(IfdApplication.class, args);
+    }
+}
